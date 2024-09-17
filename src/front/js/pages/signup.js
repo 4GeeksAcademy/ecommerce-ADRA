c272de5eb069aabe4f09c379532575ae374c5095
Alexis Rosquete <alexis.rosquete@gmail.com>
2024-09-17 08:33:28 +0000

    act singup

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -18,6 +18,15 @@ export const Signup = () => {
                     />
                     <label for="floatingInput">Email address</label>
                 </div>
+                <div class="form-floating mb-3">
+                    <input
+                        type="password"
+                        class="form-control"
+                        id="floatingPassword"
+                        placeholder="Password"
+                    />
+                    <label for="floatingPassword">Password</label>
+                </div>
                 <div class="form-floating">
                     <input
                         type="password"
